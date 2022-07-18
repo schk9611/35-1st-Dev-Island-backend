f09f408b6af7124b7e6a1235864b5ec5d6a86f13
gyuchan <schk9611@gmail.com>
2022-07-18 22:30:51 +0900

    Add: users app의  LogInView 클래스 작성

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import LogInView
+
+urlpatterns = [
+    path('/login', LogInView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+import bcrypt
+import jwt
+from django.http import JsonResponse
+from django.views import View
+from django.conf import settings
+
+from users.models import User
+
+class LogInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(email=data['email'])
+
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+            access_token = jwt.encode({'id': user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+            return JsonResponse({'message':'SUCCESS', 'ACCESS_TOKEN':access_token}, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'message':'JSONDecodeError'}, status=404)
+        except User.DoesNotExist:
+            return JsonResponse({'message':'DoesNotExist'}, status=404)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
