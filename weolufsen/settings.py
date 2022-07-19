882dbda61ecbfbd4f46761c9748509760258bdf7
seoyun <dhn04100@gmail.com>
2022-07-19 16:37:02 +0900

    Add: carts modeling

--- weolufsen/settings.py
+++ weolufsen/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'users'
+    'users',
+    'products',
+    'carts',
 ]
 
 MIDDLEWARE = [
