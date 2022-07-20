5636138881096fa60eec307114331be33495981b
seoyun <dhn04100@gmail.com>
2022-07-20 16:48:37 +0900

    Modify: orders models 수정, products views에 product_image 추가

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls    import path
 from products.views import DetailView
 
 urlpatterns = [
-    path('/detail/<product_id>', DetailView.as_view()),
+    path('/detail/<int:product_id>', DetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -3,19 +3,21 @@ import json
 from django.http       import JsonResponse
 from django.views      import View
 
-from products.models   import Product
+from products.models   import Product, ProductImage
 class DetailView(View):
     def get(self, request, product_id):
         try:
             product = Product.objects.get(id = product_id)
-            product_list = {
-                'name'       : product.id,
-                'description': product.description,
-                'content_url': product.content_url,
-                'price'      : product.price,
-                'stock'      : product.stock
+            product_detail = {
+                'id'           : product.id,
+                'name'         : product.name,
+                'product_image': [product.image_url for product in product.productimage_set.all()],
+                'description'  : product.description,
+                'content_url'  : product.content_url,
+                'price'        : product.price,
+                'stock'        : product.stock
             }
-            return JsonResponse({"result" : product_list}, status=200)
+            return JsonResponse({"result" : product_detail}, status=200)
         
         except Product.DoesNotExist:
             return JsonResponse({"message" : "DoesNotExist"}, status=400)
