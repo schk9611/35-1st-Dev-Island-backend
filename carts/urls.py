621026bc1d126203f6867b27242a4e7cf5e6ae26
seoyun <dhn04100@gmail.com>
2022-07-21 22:56:27 +0900

    Add: 회원 carts POST, GET 구현 완료

--- carts/urls.py
+++ carts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django.views import View
+
+from carts.views import CartView
 
 urlpatterns = [
+    path('', CartView.as_view())
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http     import JsonResponse
+from django.views    import View
+
+from carts.models    import Cart
+from users.utils     import signin_decorator
+from users.views     import LogInView
+from products.models import Product
+from users.models    import User
+
+
+class CartView(View):
+    @signin_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            quantity = data['quantity']
+            product = Product.objects.get(id=data['product_id'])
+            user = request.user
+            
+            Cart.objects.create(
+                quantity = quantity,
+                product = product,
+                user = user
+            )
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+        
+        except json.JSONDecodeError:
+            return JsonResponse({'message':'JSONDecodeError'}, status=404)
+
+    @signin_decorator
+    def get(self, request):
+        cart_products = [[cart.product.name, cart.quantity, cart.product.price] for cart in Cart.objects.filter(user=request.user)]
+        return JsonResponse({"cart" : cart_products}, status=200)
