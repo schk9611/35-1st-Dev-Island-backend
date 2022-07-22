6c0d9236cfefdcfb15d905dcf2f267ff7d3042bf
gyuchan <schk9611@gmail.com>
2022-07-22 21:52:17 +0900

    Modify: 코드리뷰 수정

--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
-from django.db.models import Q
-
 from django.http  import JsonResponse
 from django.views import View
 
@@ -7,30 +5,31 @@ from products.models import Product, Category, SubCategory
 
 class ProductListView(View):
     def get(self, request, category):
+        try:
+            show        = request.GET.get('show', 'all')
+            sort_method = request.GET.get('sort_method', '-release_date')
+            limit       = int(request.GET.get('limit', 10))
+            offset      = int(request.GET.get('offset', 0))
 
-        show_list    = request.GET.get('show', 'all')
-        order_method = request.GET.get('sort_method', '-release_date')
-        limit        = int(request.GET.get('limit', 10))
-        offset       = int(request.GET.get('offset', 0))
+            if show == 'all':
+                category_id = Category.objects.get(name=category).id
+                products    = Product.objects.filter(sub_category__category_id=category_id).order_by(sort_method)
+            else:
+                sub_cate_id = SubCategory.objects.get(name=show).id
+                products    = Product.objects.filter(sub_category_id=sub_cate_id).order_by(sort_method)
+
+            res_products = [{
+                    'id'          : product.id,
+                    'name'        : product.name,
+                    'description' : product.description,
+                    'price'       : product.price,
+                    'image_url'   : [image.image_url for image in product.productimage_set.all()],
+                    'release_date': product.release_date,
+                } for product in products[offset:offset+limit]] 
+
+            return JsonResponse({'RESULT':res_products}, status=200)
         
-        products     = []
-        res_products = []
-
-        if show_list == 'all':
-            category_id = Category.objects.get(name=category).id
-            products = Product.objects.filter(sub_category__category_id=category_id).order_by(order_method)
-        else:
-            sub_cate_id = SubCategory.objects.get(name=show_list).id
-            products = Product.objects.filter(sub_category_id=sub_cate_id).order_by(order_method)
-
-        for product in products[offset:offset+limit]:
-            res_products.append({
-                'id'          : product.id,
-                'name'        : product.name,
-                'description' : product.description,
-                'price'       : product.price,
-                'image_url'   : [image.image_url for image in product.productimage_set.all()],
-                'release_date': product.release_date,
-            })
-
-        return JsonResponse({'RESULT':res_products})
+        except Category.DoesNotExist:
+            return JsonResponse({'message':'CATEGORY_DOES_NOT_EXIST'}, status=400)
+        except SubCategory.DoesNotExist:
+            return JsonResponse({'message':'SUB_CATEGORY_DOES_NOT_EXIST'}, status=400)
