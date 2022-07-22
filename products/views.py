951e8ef0d1142a48dad46674ac56300a1c9693f0
gyuchan <schk9611@gmail.com>
2022-07-22 14:07:30 +0900

    Modify: 3단계 확인 구성

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from django.http  import JsonResponse
 from django.views import View
 
@@ -6,26 +8,26 @@ from products.models import Product, Category, SubCategory
 class ProductListView(View):
     def get(self, request, category):
 
-        order_method = request.GET.get('sort_method', 'release_date')
-        limit        = int(request.GET.get('limit', 10))
-        offset       = int(request.GET.get('offset', 0))
+        show_list    = request.GET.get('show', 'home_audio') # all, portable, homeaudio
+        order_method = request.GET.get('sort_method', '-price') # price, -price,   release_date, -release_date
+        limit        = int(request.GET.get('limit', 3)) # 한 페이지에 보여줄 양
+        offset       = int(request.GET.get('offset', 0)) # 보내질 데이터의 시작
 
-        sub_categories   = Category.objects.get(name=category).subcategory_set.all()
+        sub_categories = Category.objects.get(name=category).subcategory_set.all()
         sub_category_ids = [sub_category.id for sub_category in sub_categories]
         
         products     = []
         res_products = []
 
-        for sub_category_id in sub_category_ids:
-            products = Product.objects.filter(sub_category_id=sub_category_id).order_by(order_method)
-            for product in products:
-                res_products.append(product)
-            products=[]
-        
-        result = []
+        if show_list == 'all':
+            products = Product.objects.filter(Q(sub_category_id=sub_category_ids[0]) | Q(sub_category_id=sub_category_ids[1])).order_by(order_method)
+        elif show_list == 'portable':
+            products = Product.objects.filter(sub_category_id=sub_category_ids[0]).order_by(order_method)
+        elif show_list == 'home_audio':
+            products = Product.objects.filter(sub_category_id=sub_category_ids[1]).order_by(order_method)
 
-        for product in res_products:
-            result.append({
+        for product in products[offset:offset+limit]:
+            res_products.append({
                 'id'          : product.id,
                 'name'        : product.name,
                 'description' : product.description,
@@ -34,4 +36,4 @@ class ProductListView(View):
                 'release_date': product.release_date,
             })
 
-        return JsonResponse({'RESULT':result})
+        return JsonResponse({'RESULT':res_products})
