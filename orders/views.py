fbb65c5d53e3740ee8b283dbe250561b0211b571
seoyun <dhn04100@gmail.com>
2022-07-22 19:37:59 +0900

    Add: order migration

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from core.models import TimeStampModel
+from orders.models import Order, OrderProduct
+from users.utils import signin_decorator
+
+"""
+post + delete(cart) - 장바구니에서 order로 추가하는 동시에, cart에서 삭제
+get - 주문 상세 페이지
+delete - order 상세 페이지에서 주문 취소
+"""
+
+@signin_decorator
+class OrderView(TimeStampModel):
+    def post(self, request):
+        try:
+            data    = json.loads(request.body)
+            cart_id = data['cart_id']
+            
+            Order.objects.create(
+                order_status_id = 1,
+                user_id         = request.user.id
+            )
+            OrderProduct.objects.create(
+                order_id = ,
+                product_id = ,
+                quantity = ,
+                order_products_status = 1
+            )
+
+        except:
+            return
