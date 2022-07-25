b09844f94a8421d2ea046781f46cbb42113d308d
seoyun <dhn04100@gmail.com>
2022-07-25 11:52:10 +0900

    Add: 주문완료시 new order 만드는 class 생성

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from carts.views import CartView
+from carts.views import CartView, CartToOrderView
 
 urlpatterns = [
     path('', CartView.as_view()),
-    path('/<int:cart_id>', CartView.as_view())
+    path('/<int:cart_id>', CartView.as_view()),
+    path('/neworder', CartToOrderView.as_view())
 ]
--- carts/views.py
+++ carts/views.py
@@ -6,6 +6,8 @@ from django.views    import View
 from carts.models    import Cart
 from users.utils     import signin_decorator
 from products.models import Product
+from orders.models   import Order, OrderProduct
+
 
 class CartView(View):
     @signin_decorator
@@ -32,6 +34,7 @@ class CartView(View):
     @signin_decorator
     def get(self, request):
         cart_products = [{
+            'id'          : cart.id,
             'product_name': cart.product.name,
             'quantity'    : cart.quantity,
             'price'       : cart.product.price,
@@ -74,3 +77,31 @@ class CartView(View):
 
         except json.JSONDecodeError:
             return JsonResponse({'message':'JSONDecodeError'}, status=404)
+
+class CartToOrder(View):
+    @signin_decorator
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            carts_id = data['carts_id']
+            
+            order = Order.objects.create(
+                order_status_id = 1,
+                user_id         = request.user.id
+            )
+
+            for cart_id in carts_id:
+                cart = Cart.objects.get(id=cart_id)
+                OrderProduct.objects.create(
+                    order_id                = order.id,
+                    product_id              = cart.product.id,
+                    quantity                = cart.quantity,
+                    order_product_status_id = 1
+                )
+            return JsonResponse({"message" : "ORDER_SUCCESS"}, status=201)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+        
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSONDecodeError'}, status=404)
