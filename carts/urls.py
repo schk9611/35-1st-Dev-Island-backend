3fd1e6083fbcc07cc733dd1ace6033eda947345d
gyuchan <schk9611@gmail.com>
2022-07-25 23:15:02 +0900

    Modify: 변경 수정

--- carts/urls.py
+++ carts/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-from django.views import View
-
-from carts.views import CartView
 
 urlpatterns = [
-    path('', CartView.as_view()),
-    path('/<int:cart_id>', CartView.as_view())
-
 ]
