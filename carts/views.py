1177807a93c89241072db247f4af7a67107c8c71
seoyun <dhn04100@gmail.com>
2022-07-26 11:23:36 +0900

    Add: Refactoring

--- carts/views.py
+++ carts/views.py
@@ -30,6 +30,9 @@ class CartView(View):
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
+        except Cart.DoesNotExist:
+            return JsonResponse({'message':'JSONDecodeError'}, status=404)
         
         except json.JSONDecodeError:
             return JsonResponse({'message':'JSONDecodeError'}, status=404)
