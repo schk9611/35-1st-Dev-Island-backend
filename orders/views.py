ca33fca7dbb6fac746eca2274ef3aaf5632684fa
gyuchan <schk9611@gmail.com>
2022-07-26 17:17:35 +0900

    Modify: 라이브 코드리뷰 후 수정

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-import datetime, re
+import re
 
 from django.http import JsonResponse
 
@@ -11,31 +11,41 @@ class OrderView(View):
     @signin_decorator
     def get(self, request):
         try:
-            order_num = re.sub(r'[^0-9]', '', str(datetime.datetime.now()))
-
-            orders_list   = []
-            products_list = []
-
-            for order in Order.objects.filter(user=request.user):
-                orders_list.append(
+            result = {
+                'id'         : request.user.id,
+                'user_name'  : request.user.first_name + ' ' + request.user.last_name,
+                'orders_list': [
                     {
-                        'id' : order.id,
-                        'order_num' : order_num,
-                        'user_name' : order.user.first_name + ' ' + order.user.last_name,
-                    }
-                )
-                for order_product in OrderProduct.objects.filter(order_id=order.id):
-                    products_list.append(
-                        {
-                            'id'              : order_product.id,
-                            'product_img'     : [img.image_url for img in order_product.product.productimage_set.all()],
-                            'product_name'    : order_product.product.name,
-                            'product_price'   : order_product.product.price,
-                            'totalprice'      : order_product.product.price * order_product.quantity,
-                            'product_quantity': order_product.quantity
-                        }
-                    )
+                        'id'          : order.id,
+                        'order_number': re.sub(r'[^0-9]', '', str(order.created_at)) + str(order.id),
+                        # 'order_total_price' : [sum+=sum for sum in Product.Object.filter()],
+                        'products'    : [
+                            {
+                                'id'          : order_product.id,
+                                'product_img' : order_product.product.productimage_set.all()[0].image_url,
+                                'product_name': order_product.product.name,
+                                'product_price' : order_product.product.price,
+                                'quantity' : order_product.quantity,
+                                'product_total_price' : order_product.product.price * order_product.quantity
+                            } for order_product in order.orderproduct_set.all()
+                        ]
+                    } for order in Order.objects.filter(user_id=request.user.id)
+                ]
+            }
 
-            return JsonResponse({'orders_list' : orders_list, 'products_list' : products_list}, status=200)
+            
+                # for order_product in OrderProduct.objects.filter(order_id=order.id):
+                #     products_list.append(
+                #         {
+                #             'id'              : order_product.id,
+                #             'product_img'     : [img.image_url for img in order_product.product.productimage_set.all()],
+                #             'product_name'    : order_product.product.name,
+                #             'product_price'   : order_product.product.price,
+                #             'totalprice'      : order_product.product.price * order_product.quantity,
+                #             'product_quantity': order_product.quantity
+                #         }
+                #     )
+
+            return JsonResponse({'RESULT' : result}, status=200)
         except JSONDecodeError:
             return JsonResponse({'message' : 'JSONDecoderError'}, status=400)
