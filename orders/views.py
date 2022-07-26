247ee1725732b34124ad377db7cc4ea63e72c908
bigfanoftim <wkdghwns91@gmail.com>
2022-07-26 15:36:07 +0900

    live-code-review

--- orders/views.py
+++ orders/views.py
@@ -13,28 +13,16 @@ class OrderView(View):
         try:
             order_num = re.sub(r'[^0-9]', '', str(datetime.datetime.now()))
 
-            orders_list   = []
-            products_list = []
-
-            for order in Order.objects.filter(user=request.user):
-                orders_list.append(
-                    {
-                        'id' : order.id,
-                        'order_num' : order_num,
-                        'user_name' : order.user.first_name + ' ' + order.user.last_name,
-                    }
-                )
-                for order_product in OrderProduct.objects.filter(order_id=order.id):
-                    products_list.append(
+            orders = [
+                {
+                    'id' : order.id,
+                    'products' : [
                         {
-                            'id'              : order_product.id,
-                            'product_img'     : [img.image_url for img in order_product.product.productimage_set.all()],
-                            'product_name'    : order_product.product.name,
-                            'product_price'   : order_product.product.price,
-                            'totalprice'      : order_product.product.price * order_product.quantity,
-                            'product_quantity': order_product.quantity
-                        }
-                    )
+                            'id' : order_product.id
+                        } for order_product in order.orderproduct_set.all()
+                    ]
+                } for order in Order.objects.filter() 
+            ]
 
             return JsonResponse({'orders_list' : orders_list, 'products_list' : products_list}, status=200)
         except JSONDecodeError:
