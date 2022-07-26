60dfb0c2a355abb5fc99cc672d47b19deeb768d4
seoyun <dhn04100@gmail.com>
2022-07-26 13:08:16 +0900

    Add: try except 문으로 예외처리

--- carts/views.py
+++ carts/views.py
@@ -80,21 +80,28 @@ class CartView(View):
 class CartToOrderView(View):
     @signin_decorator
     def post(self,request):
-        data  = json.loads(request.body)
-        carts = Cart.objects.filter(id__in=data['cart_ids'])
-        
-        order = Order.objects.create(
-            order_status_id = 1,
-            user_id         = request.user.id
-        )
-
-        for cart in carts:
-            OrderProduct.objects.create(
-                order_id                = order.id,
-                product_id              = cart.product_id,
-                quantity                = cart.quantity,
-                order_product_status_id = 1
+        try:
+            data  = json.loads(request.body)
+            carts = Cart.objects.filter(id__in=data['cart_ids'])
+            
+            order = Order.objects.create(
+                order_status_id = 1,
+                user_id         = request.user.id
             )
-        Cart.objects.filter(user=request.user, id__in=data['cart_ids']).delete()
 
-        return JsonResponse({"message" : "NEW_ORDER_CREATED"}, status=201)
+            for cart in carts:
+                OrderProduct.objects.create(
+                    order_id                = order.id,
+                    product_id              = cart.product_id,
+                    quantity                = cart.quantity,
+                    order_product_status_id = 1
+                )
+
+            Cart.objects.filter(user=request.user, id__in=data['cart_ids']).delete()
+            return JsonResponse({"message" : "NEW_ORDER_CREATED"}, status=201)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'message':'JSONDecodeError'}, status=404)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
