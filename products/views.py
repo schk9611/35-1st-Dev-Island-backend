7bc585a52e45825c0de9da7e010ea44a38789769
gyuchan <schk9611@gmail.com>
2022-07-27 10:57:34 +0900

    Modify: listpage 완전제거

--- products/views.py
+++ products/views.py
@@ -12,7 +12,6 @@ class ProductListView(View):
             sort_method   = request.GET.get('sort_method', '-release_date')
             limit         = int(request.GET.get('limit', 16))
             offset        = int(request.GET.get('offset', 0))
-            # listType      = str(request.GET.get('listType', 9))
 
             q = Q()
 
@@ -33,7 +32,6 @@ class ProductListView(View):
                     'price'       : product.price,
                     'image_url'   : [image.image_url for image in product.productimage_set.all()],
                     'release_date': product.release_date,
-                    # 'listType'    : listType
                 } for product in products_list
             ]
 
