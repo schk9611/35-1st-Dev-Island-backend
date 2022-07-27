dc653be1bc0a7ad649aa391b63569e811ed8493c
gyuchan <schk9611@gmail.com>
2022-07-27 11:30:46 +0900

    Modify: total 금액 구현 중지

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import re
 
 from django.http import JsonResponse
 
 from django.views  import View
 from requests      import JSONDecodeError
-from orders.models import Order, OrderProduct
+from orders.models import Order
 from users.utils   import signin_decorator
 
 class OrderView(View): 
@@ -33,49 +32,7 @@ class OrderView(View):
                     } for order in Order.objects.filter(user_id=request.user.id)
                 ]
             }
-            
+
             return JsonResponse({'RESULT' : result}, status=200)
         except JSONDecodeError:
             return JsonResponse({'message' : 'JSONDecoderError'}, status=400)
-=======
-import json
-
-from django.http     import JsonResponse
-from django.views    import View
-from django.db       import transaction
-
-from carts.models    import Cart
-from users.utils     import signin_decorator
-from orders.models   import Order, OrderProduct
-
-
-class OrderView(View):
-    @signin_decorator
-    def post(self,request):
-        try:
-            data  = json.loads(request.body)
-            carts = Cart.objects.filter(id__in=data['cart_ids'])
-            
-            with transaction.atomic():
-                order = Order.objects.create(
-                    order_status_id = 1,
-                    user_id         = request.user.id
-                )
-                order_product_list = [OrderProduct(
-                    order_id                = order.id,
-                    product_id              = cart.product_id,
-                    quantity                = cart.quantity,
-                    order_product_status_id = 1
-                    )for cart in carts]
-                
-                OrderProduct.objects.bulk_create(order_product_list)
-
-                Cart.objects.filter(user=request.user, id__in=data['cart_ids']).delete()
-                return JsonResponse({"message" : "NEW_ORDER_CREATED"}, status=201)
-
-        except json.JSONDecodeError:
-            return JsonResponse({'message':'JSONDecodeError'}, status=404)
-
-        except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
->>>>>>> main
